15bb0a84ebe542df6c83e571bdd29ffde43ca54d
RikishK <69884402+RikishK@users.noreply.github.com>
2023-10-20 17:23:24 +1100

    fix (#5)
    Co-authored-by: RikishK <rikish.kamboj@student.uts.edu.au>

--- Assets/Scripts/Interactables/Stations/Bullet_Maker.cs
+++ Assets/Scripts/Interactables/Stations/Bullet_Maker.cs
@@ -2,76 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet_Maker : Interactable
+public class Bullet_Maker : Station
 {
-    private int bullet_shell_count = 0;
-    private int metal_count = 0;
-    private int max_metal = 3;
-    private int max_bullet_shell = 3;
-    private bool currentlyCrafting = false;
-
-    [SerializeField] private SpriteRenderer MetalLight, Bullet_ShellLight;
-    [SerializeField] private Sprite emptyLight, lowLight, mediumLight, fullLight;
-    [SerializeField] private Animator anim;
     public override ItemData.ItemType Interaction(CharacterController player){
 
         Debug.Log("Interacting with Bullet_Maker");
+        ItemData.ItemType heldItem = player.GetHeldItem();
 
-        if(player.GetHeldItem() == ItemData.ItemType.None){
-            if(bullet_shell_count > 0){
-                bullet_shell_count--;
+        if(heldItem == ItemData.ItemType.None){
+            if(craftedItems[ItemData.ItemType.Bullet_Shell] > 0){
+                craftedItems[ItemData.ItemType.Bullet_Shell] -= 1;
                 updateLights();
                 return ItemData.ItemType.Bullet_Shell;
             }
         }
-
-        if(player.GetHeldItem() != ItemData.ItemType.Metal) return 0;
-        if(metal_count < max_metal){
-            player.ConsumeItem();
-            metal_count++;
-            updateLights();
+        else if(validItem(heldItem)){
+            if(ingredients[heldItem] < maxIngredients[heldItem]){
+                Debug.Log($"Taking player item: {heldItem}");
+                ingredients[player.ConsumeItem()] += 1;
+                DebugInventory();
+                updateLights();
+            }
         }
 
         return ItemData.ItemType.None;
 
     }
-
-    private void updateLights(){
-        MetalLight.sprite = getLightLevel(metal_count, max_metal);
-        Bullet_ShellLight.sprite = getLightLevel(bullet_shell_count, max_bullet_shell);
+    protected override bool canCraft(){    
+        bool c = ingredients[ItemData.ItemType.Metal] > 0;
+        bool v = craftedItems[ItemData.ItemType.Bullet_Shell] < maxCraftedItems[ItemData.ItemType.Bullet_Shell] && !currentlyCrafting;
+        return c && v;
     }
 
-    private Sprite getLightLevel(int curr, int max){
-        float percentage = curr / max;
-        if(curr == 0) return emptyLight;
-        if(curr == max) return fullLight;
-        if(percentage < 0.3f) return lowLight;
-        return mediumLight;
-    }
-
-    private void Update() {
-        // Sink
-        if(!canCraft()) return;
-
-        craft();
-    }
-    private bool canCraft(){    
-        return metal_count > 0 && bullet_shell_count < max_bullet_shell && !currentlyCrafting;
-    }
-
-    private void craft(){
+    protected override void craft(){
         Debug.Log("Crafting...");
-        metal_count--;
-        StartCoroutine(craftBulletShell());
+        Debug.Log("Crafting...");
+        ingredients[ItemData.ItemType.Metal] -= 1;
+        StartCoroutine(craftItem(ItemData.ItemType.Bullet_Shell));
     }
 
-    private IEnumerator craftBulletShell(){
-        currentlyCrafting = true;
-        anim.SetBool("smashing", true);
-        yield return new WaitForSeconds(5f);
-        currentlyCrafting = false;
-        anim.SetBool("smashing", false);
-        bullet_shell_count += 1;
-        updateLights();
-    }
 }
