92715bd32989b0f9cbfcfa6d436adb9ee7a4d8b5
RikishK <rikish.kamboj@student.uts.edu.au>
2023-10-21 15:28:12 +1100

    buy upgrades

--- Assets/Scripts/LevelFinishManager.cs
+++ Assets/Scripts/LevelFinishManager.cs
@@ -7,6 +7,8 @@ public class LevelFinishManager : MonoBehaviour
 {
     [SerializeField] private GameObject upgradeableGUI;
     [SerializeField] private UpgradeableUI upgradeableUI;
+
+    private UpgradeablesData.Upgradeable currentUpgradeable;
     
     public void ShowMenu(){
         upgradeableGUI.SetActive(true);
@@ -19,6 +21,19 @@ public class LevelFinishManager : MonoBehaviour
     public void SetupMenu(UpgradeablesData.Upgradeable upgradeable){
         // TODO: Grab sprite
         Debug.Log("Setting up");
+        currentUpgradeable = upgradeable;
         upgradeableUI.Setup(null, upgradeable);
+    }
+
+    public void BuyUpgrade(string upgrade_name){
+        UpgradeablesData.UpgradeData upgradeData = currentUpgradeable.GetUpgradeData(upgrade_name);
+        if(upgradeData.upgrade_cost > GameDetails.player_money){
+            // Play an animation for cant afford
+            return;
+        }
+        Debug.Log($"Buying Upgrade {upgrade_name} of {currentUpgradeable.upgradeable_name}");
+        if (upgradeData.upgrades_done == upgradeData.total_upgrades) return;
+        upgradeData.upgrades_done += 1;
+        // TODO: Take away money
     }
 }
